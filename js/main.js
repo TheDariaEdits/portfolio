902ddf2713b586ab200423e15f61af2928cc9d65
Daria Malone <thefashionphysician@gmail.com>
2022-07-19 22:41:56 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const log = document.querySelectorAll('#logo path')
+
+for (let i = 0; i < log.length; i++) {
+    console.log(`Letter ${i} is ${log[i].getTotalLength()}`)
+    
+}
+
+Letter 0 is 362.32000732421875
+Letter 1 is 438.7596130371094
+Letter 2 is 479.0799865722656
+Letter 3 is 572.6865844726562
+Letter 4 is 510.34228515625
+Letter 5 is 574.5406494140625
+Letter 6 is 195.43991088867188
+Letter 7 is 510.3423156738281
+Letter 8 is 577.0800170898438
+Letter 9 is 461.0752868652344
+Letter 10 is 195.44003295898438
+Letter 11 is 300.7140808105469
+Letter 12 is 410.9345703125
